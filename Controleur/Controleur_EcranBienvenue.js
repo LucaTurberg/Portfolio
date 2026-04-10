8f03abc8bed98917b37dd327e784ffb820e23acf
LucaTurberg <luca.turberg@outlook.fr>
2026-04-10 21:52:34 +0200

    Update Controleur_EcranBienvenue.js

--- Controleur/Controleur_EcranBienvenue.js
+++ Controleur/Controleur_EcranBienvenue.js
@@ -1,3 +1,14 @@
 fetch("Vue/Vue_EcranBienvenue.html")
   .then(r => r.text())
-  .then(html => document.body.innerHTML = html);
+  .then(html => {
+
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "../Style/Style_EcranBienvenue.css";
+
+    link.onload = () => {
+      document.body.innerHTML = html;
+    };
+
+    document.head.appendChild(link);
+  });
