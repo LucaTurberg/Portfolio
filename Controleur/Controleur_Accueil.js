87919fdd90a9e64c7ac5c0aacf19dddcbe7fb389
LucaTurberg <luca.turberg@outlook.fr>
2026-04-10 23:19:04 +0200

    Update Controleur_Accueil.js

--- Controleur/Controleur_Accueil.js
+++ Controleur/Controleur_Accueil.js
@@ -1,11 +1,21 @@
-class Controleur_Accueil() {
-  async chargerAccueil() {
-    const [entete, accueil, pied] = await Promise.all([
-      fetch("Vue/Vue_Entete.html").then((r) => r.text()),
-      fetch("Vue/Vue_Accueil.html").then((r) => r.text()),
-      fetch("Vue/Vue_Pied.html").then((r) => r.text()),
-    ]);
+Promise.all([
+  fetch("Vue/Vue_Entete.html").then(r => r.text()),
+  fetch("Vue/Vue_Accueil.html").then(r => r.text()),
+  fetch("Vue/Vue_Pied.html").then(r => r.text())
+])
+.then(([entete, accueil, pied]) => {
 
-    document.innerHTML = entete + accueil + pied;
-  }
-}
+  // Charger le CSS
+  const css = document.createElement("link");
+  css.rel = "stylesheet";
+  css.href = "Style/Style_Accueil.css";
+
+  css.onload = () => {
+
+    // Injecter les 3 vues dans le body
+    document.body.innerHTML = entete + accueil + pied;
+
+  };
+
+  document.head.appendChild(css);
+});
