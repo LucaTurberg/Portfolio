e46b0d3bb5e93fa22d2f8cab9d412e08589e5d3e
LucaTurberg <luca.turberg@outlook.fr>
2026-04-10 15:47:45 +0200

    Update Controleur_EcranBienvenue.js

--- Controleur/Controleur_EcranBienvenue.js
+++ Controleur/Controleur_EcranBienvenue.js
@@ -3,7 +3,11 @@ class Controleur_EcranBienvenue {
     try {
       const response = await fetch("Vue/Vue_EcranBienvenue.html");
       const html = await response.text();
-
+      document.open(); 
+      document.write(html); 
+      document.close(); // 👉 Affiche la page une fois tout chargé 
+      
+      document.body.classList.add("loaded");
 
     } catch (error) {
       console.error("Erreur lors du chargement :", error);
