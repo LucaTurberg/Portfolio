bc591b36ee5a8d62c309af66dfa39dd5e3375285
LucaTurberg <luca.turberg@outlook.fr>
2026-04-10 15:31:28 +0200

    Update Controleur_Accueil.js

--- Controleur/Controleur_Accueil.js
+++ Controleur/Controleur_Accueil.js
@@ -1,11 +1,21 @@
-class Controleur_Accueil() {
+class Controleur_Accueil {
   async chargerAccueil() {
-    const [entete, accueil, pied] = await Promise.all([
-      fetch("Vue/Vue_Entete.html").then((r) => r.text()),
-      fetch("Vue/Vue_Accueil.html").then((r) => r.text()),
-      fetch("Vue/Vue_Pied.html").then((r) => r.text()),
-    ]);
+    try {
+      const [entete, accueil, pied] = await Promise.all([
+        fetch("Vue/Vue_Entete.html").then((r) => r.text()),
+        fetch("Vue/Vue_Accueil.html").then((r) => r.text()),
+        fetch("Vue/Vue_Pied.html").then((r) => r.text()),
+      ]);
 
-    document.innerHTML = entete + accueil + pied;
+      // Assemblage du HTML
+      const html = entete + accueil + pied;
+
+      document.open();
+      document.write(html);
+      document.close();
+
+    } catch (error) {
+      console.error("Erreur lors du chargement :", error);
+    }
   }
 }
