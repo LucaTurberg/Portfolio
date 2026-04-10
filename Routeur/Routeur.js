1d7a199950056dc571b1fabb4dbff562f6ae7cca
LucaTurberg <luca.turberg@outlook.fr>
2026-04-10 20:20:59 +0200

    Update Routeur.js

--- Routeur/Routeur.js
+++ Routeur/Routeur.js
@@ -1,7 +1,11 @@
 class Router {
-  EcranBienvenue() { window.location.hash = "#EcranBienvenue", import("../Controleur/Controleur_EcranBienvenue.js"); }
-
-  Accueil() { window.location.hash = "#Accueil", import("../Controleur/Controleur_Accueil.js"); }
-
-  Contact() { window.location.hash = "#Contact", import("../Controleur/Controleur_Contact.js"); }
+  EcranBienvenue() { 
+    window.location.hash = "#EcranBienvenue", import("../Controleur/Controleur_EcranBienvenue.js"); 
+  }
+  Accueil() {
+    window.location.hash = "#Accueil", import("../Controleur/Controleur_Accueil.js"); 
+  }
+  Contact() {
+    window.location.hash = "#Contact", import("../Controleur/Controleur_Contact.js"); 
+  }
 }
