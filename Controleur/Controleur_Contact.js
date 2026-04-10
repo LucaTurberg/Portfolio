a8db8180cedd53193902d3c242f38023797192fa
LucaTurberg <luca.turberg@outlook.fr>
2026-04-10 23:19:40 +0200

    Update Controleur_Contact.js

--- Controleur/Controleur_Contact.js
+++ Controleur/Controleur_Contact.js
@@ -1,11 +1,21 @@
-class Controleur_Contact {
-  async chargerContact() {
-    const [entete, contact, pied] = await Promise.all([
-      fetch("Vue/Vue_Entete.html").then((r) => r.text()),
-      fetch("Vue/Vue_Contact.html").then((r) => r.text()),
-      fetch("Vue/Vue_Pied.html").then((r) => r.text()),
-    ]);
+Promise.all([
+  fetch("Vue/Vue_Entete.html").then(r => r.text()),
+  fetch("Vue/Vue_Contact.html").then(r => r.text()),
+  fetch("Vue/Vue_Pied.html").then(r => r.text())
+])
+.then(([entete, contact, pied]) => {
 
-    document.innerHTML = entete + contact + pied;
-  }
-}
+  // Charger le CSS
+  const css = document.createElement("link");
+  css.rel = "stylesheet";
+  css.href = "Style/Style_Contact.css";
+
+  css.onload = () => {
+
+    // Injecter les 3 vues dans le body
+    document.body.innerHTML = entete + contact + pied;
+
+  };
+
+  document.head.appendChild(css);
+});
