98ff8423f6ac57780a990ef6354baeeb591c272d
LucaTurberg <luca.turberg@outlook.fr>
2026-04-27 13:45:38 +0200

    Update Controleur_Contact.js

--- Controleur/Controleur_Contact.js
+++ Controleur/Controleur_Contact.js
@@ -1,23 +1,27 @@
 Promise.all([
+  // 1. Charger toutes les vues HTML
   fetch("Vue/Vue_Entete.html").then(r => r.text()),
   fetch("Vue/Vue_Contact.html").then(r => r.text()),
   fetch("Vue/Vue_Pied.html").then(r => r.text())
 ])
 .then(([entete, contact, pied]) => {
 
+  // 2. Ouvrir le document pour réécrire toute la page
   document.open();
 
-  // CSS
+  // 3. Charger le CSS en premier pour appliquer le style dès l’affichage
   document.write('<link rel="stylesheet" href="Style/Style_Contact.css">');
 
-  // Scripts (src)
- // document.write('<script src="Routeur/Routeur.js"><\/script>');
-  // document.write('<script src="Model/Model_Entete.js"><\/script>');
-
-  // Vues
+  // 4. Insérer le HTML dans l’ordre d’affichage de la page
+  // Entête -> Contenu principal -> Pied de page
   document.write(entete);
   document.write(contact);
   document.write(pied);
 
+  // 5. Charger les scripts en dernier
+  // Important : le HTML doit déjà exister pour que getElementById fonctionne
+  document.write('<script src="Model/Model_Entete.js"><\/script>');
+
+  // 6. Fermer l’écriture du document
   document.close();
 });
