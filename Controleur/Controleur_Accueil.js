3085e4e3d259e3730e7da6fae1887b27f7f3ec4d
LucaTurberg <luca.turberg@outlook.fr>
2026-04-27 13:59:33 +0200

    Update Controleur_Accueil.js

--- Controleur/Controleur_Accueil.js
+++ Controleur/Controleur_Accueil.js
@@ -6,42 +6,22 @@ Promise.all([
 ])
 .then(([entete, accueil, pied]) => {
 
-  // 2. Effacer complètement le contenu actuel de la page
-  document.body.innerHTML = "";
-  document.head.innerHTML = "";
-
-  // 3. Ouvrir le document pour réécrire toute la page
+  // 2. Ouvrir le document pour réécrire toute la page
   document.open();
 
-  // 4. Recréer la structure HTML + charger le CSS en premier
-  document.write(`
-    <!DOCTYPE html>
-    <html lang="fr">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="Style/Style_Accueil.css">
-      <title>Accueil</title>
-    </head>
-    <body>
-  `);
+  // 3. Charger le CSS en premier pour appliquer le style dès l’affichage
+  document.write('<link rel="stylesheet" href="Style/Style_Accueil.css">');
 
-  // 5. Insérer le HTML dans l’ordre d’affichage
+  // 4. Insérer le HTML dans l’ordre d’affichage de la page
   // Entête -> Accueil -> Pied de page
   document.write(entete);
   document.write(accueil);
   document.write(pied);
 
-  // 6. Charger les scripts en dernier
-  // Important : le HTML doit déjà exister pour utiliser getElementById
+  // 5. Charger les scripts en dernier
+  // Important : le HTML doit déjà être chargé pour utiliser getElementById
   document.write('<script src="Model/Model_Entete.js"><\/script>');
 
-  // 7. Fermer body + html
-  document.write(`
-    </body>
-    </html>
-  `);
-
-  // 8. Fermer l’écriture du document
+  // 6. Fermer l’écriture du document
   document.close();
 });
