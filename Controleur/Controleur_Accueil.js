29cbd8dd4045db863b65f3eac44e5dda0a2cdc6c
LucaTurberg <luca.turberg@outlook.fr>
2026-04-27 13:53:47 +0200

    Update Controleur_Accueil.js

--- Controleur/Controleur_Accueil.js
+++ Controleur/Controleur_Accueil.js
@@ -1,22 +1,27 @@
 Promise.all([
+  // 1. Charger toutes les vues HTML
   fetch("Vue/Vue_Entete.html").then(r => r.text()),
   fetch("Vue/Vue_Accueil.html").then(r => r.text()),
   fetch("Vue/Vue_Pied.html").then(r => r.text())
 ])
 .then(([entete, accueil, pied]) => {
 
+  // 2. Ouvrir le document pour réécrire toute la page
   document.open();
 
-  // CSS
+  // 3. Charger le CSS en premier pour appliquer le style dès l’affichage
   document.write('<link rel="stylesheet" href="Style/Style_Accueil.css">');
 
-  // Vues
+  // 4. Insérer le HTML dans l’ordre d’affichage de la page
+  // Entête -> Accueil -> Pied de page
   document.write(entete);
   document.write(accueil);
   document.write(pied);
 
-  // Scripts (en dernier, après le HTML)
+  // 5. Charger les scripts en dernier
+  // Important : le HTML doit déjà être chargé pour utiliser getElementById
   document.write('<script src="Model/Model_Entete.js"><\/script>');
 
+  // 6. Fermer l’écriture du document
   document.close();
 });
